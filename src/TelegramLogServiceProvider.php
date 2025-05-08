90acb91b9e87f384403204b1a755a808e65a9295
doker42 <doker42@gmail.com>
2025-05-08 21:26:54 +0300

    config

---
+++ src/TelegramLogServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Doker42\Telegramlog;
+
+use Illuminate\Support\ServiceProvider;
+
+class TelegramLogServiceProvider  extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/telegramlog.php',
+            'telegramlog'
+        );
+    }
+
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__ . '/../config/telegramlog.php' => config_path('telegramlog.php'),
+        ], 'telegramlog');
+    }
+}
